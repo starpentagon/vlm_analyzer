a783310234b35a4e2a238f3676bdfb05e08d5c22
starpentagon <starpentagon@gmail.com>
2017-05-26 11:55:03 +0900

    Add SimulationTest.

--- test/VLMAnalyzerTest.cc
+++ test/VLMAnalyzerTest.cc
@@ -52,8 +52,9 @@ public:
       MoveList move_list("hhhgihghjhggkh");
       VLMAnalyzer vlm_analyzer(move_list, vlm_table);
 
+      VLMSearch vlm_search;
       MoveList candidate_move;
-      vlm_analyzer.GetCandidateMoveOR<kWhiteTurn>(&candidate_move);
+      vlm_analyzer.GetCandidateMoveOR<kWhiteTurn>(vlm_search, &candidate_move);
 
       ASSERT_EQ(1, candidate_move.size());
       ASSERT_EQ(kMoveLH, candidate_move[0]);
@@ -80,8 +81,9 @@ public:
       MoveList move_list("hhhgihghjhgg");
       VLMAnalyzer vlm_analyzer(move_list, vlm_table);
 
+      VLMSearch vlm_search;
       MoveList candidate_move;
-      vlm_analyzer.GetCandidateMoveOR<kBlackTurn>(&candidate_move);
+      vlm_analyzer.GetCandidateMoveOR<kBlackTurn>(vlm_search, &candidate_move);
 
       ASSERT_EQ(219, candidate_move.size());
     }
@@ -108,11 +110,13 @@ public:
       // N | . . . . . . . . . . . . . | N 
       // O + --------------------------+ O 
       //   A B C D E F G H I J K L M N O 
-      MoveList move_list("hhhgihghjhggkh");
+      MoveList move_list("hhhgihghjhgg");
       VLMAnalyzer vlm_analyzer(move_list, vlm_table);
+      vlm_analyzer.MakeMove(kMoveKH);
 
+      VLMSearch vlm_search;
       MoveList candidate_move;
-      vlm_analyzer.GetCandidateMoveAND<kWhiteTurn>(&candidate_move);
+      vlm_analyzer.GetCandidateMoveAND<kWhiteTurn>(vlm_search, &candidate_move);
 
       ASSERT_EQ(1, candidate_move.size());
       ASSERT_EQ(kMoveLH, candidate_move[0]);
@@ -136,11 +140,13 @@ public:
       // N | . . . . . . . . . . . . . | N 
       // O + --------------------------+ O 
       //   A B C D E F G H I J K L M N O 
-      MoveList move_list("hhhgihghjhgg");
+      MoveList move_list("hhhgihghjh");
       VLMAnalyzer vlm_analyzer(move_list, vlm_table);
+      vlm_analyzer.MakeMove(kMoveGG);
 
+      VLMSearch vlm_search;
       MoveList candidate_move;
-      vlm_analyzer.GetCandidateMoveAND<kBlackTurn>(&candidate_move);
+      vlm_analyzer.GetCandidateMoveAND<kBlackTurn>(vlm_search, &candidate_move);
 
       ASSERT_EQ(219 + 1, candidate_move.size());    // Passを含む
     }
@@ -153,6 +159,102 @@ public:
     ASSERT_EQ(kVLMStrongDisproved, vlm_analyzer.GetSearchValue(kVLMStrongDisproved));
     ASSERT_EQ(kVLMWeakDisprovedUB - 1, vlm_analyzer.GetSearchValue(kVLMWeakDisprovedUB));
     ASSERT_EQ(kVLMProvedUB - 1, vlm_analyzer.GetSearchValue(kVLMProvedUB));
+  }
+
+  void SimulationTest()
+  {
+    //   A B C D E F G H I J K L M N O 
+    // A o o ------------------------+ A 
+    // B o o . . . . . . . . . . . . | B 
+    // C | . . . . . . . . . . . . . | C 
+    // D | . . * . . . . . . . * . . | D 
+    // E | . . . . . . . . . . . . . | E 
+    // F | . . . . x . . . . . x . . | F 
+    // G | . . . . x . o o . . x . . | G 
+    // H | . . . . . . x x x . . . . | H 
+    // I | . . . . . . . . . . . . . | I 
+    // J | . . . . . . . . . . . . . | J 
+    // K | . . . . . . . . . . . . . | K 
+    // L | . . * . . . . . . . * . . | L 
+    // M | . . . . . . . . . . . . . | M 
+    // N | . . . . . . . . . . . . . | N 
+    // O + --------------------------+ O 
+    //   A B C D E F G H I J K L M N O 
+    MoveList move_list("hhhgihigjhaafgabffbalgbb");
+    VLMAnalyzer vlm_analyzer(move_list, vlm_table);
+    vlm_analyzer.MakeMove(kMoveLF);
+
+    // Passをして詰む手順を求める
+    vlm_analyzer.MakeMove(kNullMove);
+    MoveTree proof_tree;
+
+    {
+      VLMSearch vlm_search;
+      vlm_search.remain_depth = 1;
+      VLMResult vlm_result;
+  
+      const auto or_node_value = vlm_analyzer.SolveOR<kBlackTurn>(vlm_search, &vlm_result);
+      ASSERT_TRUE(IsVLMProved(or_node_value));
+
+      const auto is_generated = vlm_analyzer.GetProofTree(&proof_tree);
+      ASSERT_TRUE(is_generated);
+      ASSERT_EQ(1, proof_tree.size());
+      ASSERT_TRUE(proof_tree.GetTopNodeMove() == kMoveGH || proof_tree.GetTopNodeMove() == kMoveKH);
+    }
+
+    vlm_analyzer.UndoMove();
+
+    // 影響領域外の手に対してはSimulationが成立する
+    vlm_analyzer.MakeMove(kMoveOO);
+    
+    {
+      VLMSearch vlm_simulation;
+      vlm_simulation.is_search = false;
+
+      const auto or_node_value = vlm_analyzer.SimulationOR<kBlackTurn>(vlm_simulation, &proof_tree);
+      ASSERT_TRUE(IsVLMProved(or_node_value));
+    }
+    
+    vlm_analyzer.UndoMove();
+
+    // 影響領域の手ではSimulationが失敗するが、別の手で詰む
+    const auto terminate_move = proof_tree.GetTopNodeMove();
+    vlm_analyzer.MakeMove(terminate_move);
+
+    {
+      VLMSearch vlm_simulation;
+      vlm_simulation.is_search = false;
+
+      const auto or_node_value = vlm_analyzer.SimulationOR<kBlackTurn>(vlm_simulation, &proof_tree);
+      ASSERT_TRUE(!IsVLMProved(or_node_value));
+    }
+    {
+      VLMSearch vlm_search;
+      vlm_search.remain_depth = 3;
+      VLMResult vlm_result;
+  
+      const auto or_node_value = vlm_analyzer.SolveOR<kBlackTurn>(vlm_search, &vlm_result);
+      ASSERT_TRUE(IsVLMProved(or_node_value));
+
+      const auto is_generated = vlm_analyzer.GetProofTree(&proof_tree);
+      ASSERT_TRUE(is_generated);
+      ASSERT_EQ(4, proof_tree.size());    // 達四＋四->四防手->達四
+    }
+
+    vlm_analyzer.UndoMove();
+
+    // いずれの四三の焦点に対してもSimulationが成立
+    for(const auto move : {kMoveFH, kMoveLH}){
+      vlm_analyzer.MakeMove(move);
+
+      VLMSearch vlm_simulation;
+      vlm_simulation.is_search = false;
+
+      const auto or_node_value = vlm_analyzer.SimulationOR<kBlackTurn>(vlm_simulation, &proof_tree);
+      ASSERT_TRUE(IsVLMProved(or_node_value));
+      
+      vlm_analyzer.UndoMove();
+    }
   }
 };
 
@@ -268,5 +370,10 @@ TEST_F(VLMAnalyzerTest, TranspositionTableTest)
 TEST_F(VLMAnalyzerTest, GetSearchValueTest)
 {
   GetSearchValueTest();
-}  
+}
+
+TEST_F(VLMAnalyzerTest, SimulationTest)
+{
+  SimulationTest();
+}
 }
